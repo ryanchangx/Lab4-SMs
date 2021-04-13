cb6f27febe26c88b103c30e10634981cad9efa8d
Ryan Chang <ryanchangx@gmail.com>
2021-04-13 12:14:50 -0700

    Initialize part 1

--- source/main.c
+++ source/main.c
@@ -12,12 +12,38 @@
 #include "simAVRHeader.h"
 #endif
 
+enum STATES{DOWN, UP, WAIT} state;
+void tick(){
+    unsigned char tmpB = PORTB;
+    switch(state){
+        case DOWN:
+            if(PORTB == 0x02)
+                PORTB = tmpB >> 1;
+            else
+                PORTB = tmpB << 1;
+            state = UP;
+            break;
+        case UP:
+            if(PINA == 0x00)
+                state = WAIT;
+            break;
+        case WAIT:
+            if(PINA == 0x01)
+                state = DOWN;
+            break;
+        default:
+            state = WAIT;
+    }
+    
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRA = 0x00; PORTA = 0x00;
+    DDRB = 0X03; PORTB = 0x01;
     /* Insert your solution below */
     while (1) {
-
+        tick();
     }
     return 1;
 }
